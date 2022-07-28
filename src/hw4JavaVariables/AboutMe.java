06c7f7dc63077caaf7d72291417c758cfbd457c9
Braiden Jake <braidenjake@Braidens-MacBook-Pro.local>
2022-07-28 17:19:14 -0400

    first commit

--- src/hw4JavaVariables/AboutMe.java
+++ src/hw4JavaVariables/AboutMe.java
@@ -1,8 +1,9 @@
 package hw4JavaVariables;
 
 public class AboutMe {
-	public String name = "Angilique";// variable initialized
-	public String name1;// variable declared
+	
+// Variables,  methods(),  constructor 
+	public String name ="Angelique" ;// variable initialized 
 	public byte age = 127;
 	public short myApartmentRent = 32767;
 	public int myYearlySalary = 1234567890;
@@ -10,14 +11,29 @@ public class AboutMe {
 	public float myHeight = 2.356789f;
 	public double myGrade = 3.2567897665;
 	public char sex = 'F';
-	public boolean usCitizen = false;
+	public boolean usCitizen = false; 
+// Constructor Declared
+	public AboutMe(){
+	System.out.println("My Information is given below");
+	
+	}
+     // method implemented
+    public void aboutMe(){
+  System.out.println("My Name is:" + name + " \nMy Age:" + age + "\n MyapartmentRent " +  myApartmentRent); 	
+	
+	
 
-	public static void main(String[] args) {
-		AboutMe angilique = new AboutMe();
-		System.out.println(angilique.name);
-		System.out.println("My Name:" + angilique.name + " \nMy Age:" + angilique.age + "\nMyapartmentRent " + angilique. myApartmentRent);
-	
-	}	
-	
+	}
+
+
+
+    
+    }
+
+
 }
+
+
+
+
 
---
+++ src/hw4JavaVariables/AboutMeTest.java
@@ -0,0 +1,18 @@
+package hw4JavaVariables;
+
+
+public class AboutMeTest {
+
+	public static void main(String[] args) {
+	AboutMe angilique = new AboutMe();
+	System.out.println(angilique.name);
+
+	
+		System.out.println(angilique.name);
+		System.out.println("My Name:" + angilique.name + " \nMy Age:" + angilique.age + "\nMyapartmentRent " + angilique. myApartmentRent); 	
+		angilique.aboutMe();
+	
+
+	}
+
+}
